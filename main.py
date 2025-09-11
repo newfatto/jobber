693d33ed4b35c3c28d29de4fb239888fcd4b2ad1
Ekaterina Chernyshova <newfatto@gmail.com>
2025-09-11 18:32:33 +0300

    Создана функция load_data_to_bd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from config import config
 from src.hh_api import get_hh_data
-from src.utils import create_db_and_tables
+from src.utils import create_db_and_tables, load_data_to_bd
 
 
 def main():
@@ -8,19 +8,20 @@ def main():
     company_list = [
         "4649269",  # T1
         "1740",  # Яндекс
-        # '2748', #Ростелеком
-        # '3529', #СБЕР
-        # '78638', #Т-банк
-        # '3776', #MTC
-        # '15478', #VK
-        # '80', #Альфа банк
-        # '4181', #ВТБ
-        # '115' #Ай-Теко
+        "2748",  # Ростелеком
+        "3529",  # СБЕР
+        "78638",  # Т-банк
+        "3776",  # MTC
+        "15478",  # VK
+        "80",  # Альфа банк
+        "4181",  # ВТБ
+        "115",  # Ай-Теко
     ]
 
     data = get_hh_data(company_list)
     params = config()
     create_db_and_tables("jobber", params)
+    load_data_to_bd("jobber", data, params)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,6 @@ import psycopg2
 from psycopg2 import errors, sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-from config import config
-from src.hh_api import get_hh_data
-
 
 def create_db_and_tables(database_name: str, params) -> None:
     """Функция создаёт базу данных и таблицы в PostgreSQL"""
@@ -29,7 +26,7 @@ def create_db_and_tables(database_name: str, params) -> None:
             cur.execute(
                 """
             CREATE TABLE employers(
-            employer_id SERIAL PRIMARY KEY,
+            employer_id BIGINT PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
             site_url TEXT,
             area VARCHAR(100),
@@ -46,7 +43,6 @@ def create_db_and_tables(database_name: str, params) -> None:
             employer_id INT REFERENCES employers(employer_id),
             salary_from INT,
             salary_to INT,
-            salary INT,
             currency VARCHAR(10),
             area VARCHAR(100),
             url TEXT
@@ -58,6 +54,43 @@ def create_db_and_tables(database_name: str, params) -> None:
         print("Созданы столбцы в таблицах")
 
 
-def load_data_to_bd():
+def load_data_to_bd(database_name, data, params):
     """Функция загружает полученные данные в базу данных"""
-    pass
+    with psycopg2.connect(database=database_name, **params) as conn:
+
+        with conn.cursor() as cur:
+            for employer in data["employers"]:
+                cur.execute(
+                    """
+                INSERT INTO employers(employer_id, name, site_url, area, open_vacancies)
+                VALUES(%s, %s, %s, %s, %s)
+                """,
+                    (
+                        int(employer["id"]),
+                        employer.get("name"),
+                        employer.get("site_url"),
+                        (employer.get("area") or {}).get("name"),
+                        employer.get("open_vacancies"),
+                    ),
+                )
+
+            for vacancy in data["vacancies"]:
+                salary = vacancy.get("salary") or {}
+                cur.execute(
+                    """
+                INSERT INTO vacancies(name, employer_id, salary_from, salary_to, currency, area, url)
+                VALUES(%s, %s, %s, %s, %s, %s, %s)
+                RETURNING vacancy_id
+                """,
+                    (
+                        vacancy.get("name"),
+                        int((vacancy.get("employer") or {}).get("id")),
+                        salary.get("from"),
+                        salary.get("to"),
+                        salary.get("currency"),
+                        (vacancy.get("area") or {}).get("name"),
+                        vacancy.get("alternate_url"),
+                    ),
+                )
+                conn.commit()
+    print("Наполнение таблиц завершено")
